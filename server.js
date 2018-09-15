0463129469103505e5cfb6d3336e884fb57da0f4
Petteri Kaikkonen <petteri.kaikkonen@gmail.com>
2018-09-16 00:11:28 +0300

    Dummy update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(new Date());
+  res.send('<h1>Hello!</h1><br>' + new Date());
 });
 
 app.listen(PORT, HOST);
